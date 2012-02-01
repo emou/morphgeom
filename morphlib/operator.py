cb78cae7aa61254d523452c8ff582033d3ed5e61
emou <emils@fmi.uni-sofia.bg>
2012-02-01 17:04:30 +0200

    Try to keep lines within 80 chars.

--- morphlib/operator.py
+++ morphlib/operator.py
@@ -97,9 +97,11 @@ class StructuralElement(object):
 
     def __init__(self, matrix_list, center=None):
         if not all(x in [0, 1] for row in matrix_list for x in row):
-            raise TypeError("Structured element should be initialized with a matrix containing only zeros and ones")
+            raise TypeError("Structured element should be initialized with a matrix "
+                            " containing only zeros and ones")
         elif not all(len(row) == len(matrix_list[0]) for row in matrix_list):
-            raise TypeError("Malformed structured element matrix: %r" % matrix_list)
+            raise TypeError(
+                "Malformed structured element matrix: %r" % matrix_list)
         self.matrix = list(matrix_list)
         if center is None:
             self.center = (len(self.matrix) / 2, len(self.matrix[0]) / 2)
